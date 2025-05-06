f136291b7d8be48d9613e477823c39644a1f3e93
BytesBuilder <41378186+oxrajesh@users.noreply.github.com>
2025-05-06 16:58:41 +0800

    WIP:: Removed about in Settings page

--- src/lib/components/chat/SettingsModal.svelte
+++ src/lib/components/chat/SettingsModal.svelte
@@ -609,7 +609,7 @@
 								</div>
 								<div class=" self-center">{$i18n.t('Account')}</div>
 							</button>
-						{:else if tabId === 'about'}
+						<!-- {:else if tabId === 'about'}
 							<button
 								class="px-0.5 py-1 min-w-fit rounded-lg flex-1 md:flex-none flex text-left transition {selectedTab ===
 								'about'
@@ -634,7 +634,7 @@
 									</svg>
 								</div>
 								<div class=" self-center">{$i18n.t('About')}</div>
-							</button>
+							</button> -->
 						{:else if tabId === 'admin'}
 							{#if $user?.role === 'admin'}
 								<button
@@ -725,9 +725,10 @@
 							toast.success($i18n.t('Settings saved successfully!'));
 						}}
 					/>
-				{:else if selectedTab === 'about'}
-					<About />
 				{/if}
+				<!-- {:else if selectedTab === 'about'}
+					<About />
+				{/if} -->
 			</div>
 		</div>
 	</div>
